d0c2b4386cfe49e2e3a19294f552a18128ef0dd6
Mainviel Abellard <MAINVIE123456789@GMAIL.COM>
2015-07-17 12:04:05 -1000

    ou pa scroll ........
    basic app

---
+++ app/src/main/java/gridimagesearch/codepath/com/gridimagesearch/activities/ImageDisplayActivity.java
@@ -0,0 +1,52 @@
+package gridimagesearch.codepath.com.gridimagesearch.activities;
+
+//import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+//import android.support.v7.app.ActionBar;
+//import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import gridimagesearch.codepath.com.gridimagesearch.R;
+import gridimagesearch.codepath.com.gridimagesearch.models.ImageResult;
+
+
+public class ImageDisplayActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_display);
+
+        ImageResult result = (ImageResult) getIntent().getSerializableExtra("result");
+        ImageView ivImageResult = (ImageView) findViewById(R.id.ivImageResult);
+        Picasso.with(this).load(result.fullUrl).into(ivImageResult);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image_display, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/gridimagesearch/codepath/com/gridimagesearch/activities/SearchActivity.java
@@ -0,0 +1,193 @@
+package gridimagesearch.codepath.com.gridimagesearch.activities;
+
+//import android.app.Activity;
+//import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+//import android.preference.PreferenceActivity;
+//import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import gridimagesearch.codepath.com.gridimagesearch.R;
+//import gridimagesearch.codepath.com.gridimagesearch.adapters.ImageResultsAdapter;
+//import gridimagesearch.codepath.com.gridimagesearch.adapters.ImageResultsAdapter;
+import gridimagesearch.codepath.com.gridimagesearch.adapters.ImageResultsAdapter;
+import gridimagesearch.codepath.com.gridimagesearch.models.ImageResult;
+
+
+public class SearchActivity extends ActionBarActivity {
+    private EditText etQuery;
+    private GridView gvResults;
+    private ArrayList<ImageResult> imageResults;
+    private ImageResultsAdapter aImageResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        setupViews();
+
+        // create the data
+        imageResults= new ArrayList<ImageResult>();
+        // attach the data created
+        aImageResults= new ImageResultsAdapter(this,imageResults);
+        // connect the adapter to adapter View
+        gvResults.setAdapter(aImageResults);
+
+
+    }
+    private void setupViews() {
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+        gvResults.setOnScrollListener(new EndlessScrollListener() {
+
+            @Override
+            public  void onLoadMore(int page, int totalItemsCount) {
+                customLoadMoreDataFromApi(true);
+            }
+        });
+        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Launch Image Display activity
+                //Creating an intent
+                Intent i = new Intent(SearchActivity.this, ImageDisplayActivity.class);
+                //Get the image result to display
+                ImageResult result = imageResults.get(position);
+                // Pass image result to the intent
+                i.putExtra("result", result);
+                //Laucnh the new activity
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    //This method will run anytime the "search button" is clicked. This is possible thanks to the onClick attribute in activity_search.xml
+    public void onImageSearch(View v) {
+        //Get the string from the EditText
+        String query = etQuery.getText().toString();
+        //Print the Text on the screen
+        Toast.makeText(this, "Search for: " + query, Toast.LENGTH_SHORT).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        //http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=android&rsz=8
+        String searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8";
+        client.get(searchUrl, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers,
+                                  JSONObject response) {
+                Log.d("DEBUG", response.toString());
+                JSONArray imageResultsJson = null;
+                try {
+                    imageResultsJson = response.getJSONObject("responseData").getJSONArray("results");
+                    Toast.makeText(getApplicationContext(), "Found Ya!", Toast.LENGTH_SHORT).show();
+                    imageResults.clear(); //clear the existig images in case there is a new search
+                    // When you make to the adapter, it does modify the underliying data auto
+                    aImageResults.addAll(ImageResult.fromJSONArray(imageResultsJson));
+                } catch (JSONException e) {
+                    //TODO catch block
+                    e.printStackTrace();
+                }
+                Log.i("INFO", imageResults.toString());
+            }
+        });
+    }
+
+    int start = 8;
+
+    public void customLoadMoreDataFromApi(final boolean isPage) {
+        // This method probably sends out a network request and appends new data items to your adapter.
+        // Use the offset value and add it as a parameter to your API request to retrieve paginated data.
+        // Deserialize API response and then construct new objects to append to the adapter
+        String query = etQuery.getText().toString();
+
+        String url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+ query +"&rsz=8" + "&start=" + start ;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.i("DEBUG", response.toString());
+                JSONArray imageResultsJson = null;
+                try {
+                    if (isPage) {
+                        imageResultsJson = response.getJSONObject("responseData").getJSONArray("results");
+                        aImageResults.addAll(ImageResult.fromJSONArray(imageResultsJson));
+                        start = start + 8;
+                    }
+                    JSONObject cursor = response.getJSONObject("responseData").getJSONObject("cursor");
+                    int currentPage = cursor.getInt("currentPageIndex");
+                    JSONArray pages = cursor.getJSONArray("page");
+                    if ((pages.length() - 1) > currentPage) {
+                        JSONObject page = pages.getJSONObject(currentPage + 1);
+                        // searchOptions.start = page.getString("start");
+                    } else if (!isPage) {
+                        // stop searching once we have reached the end
+                        Toast.makeText(getApplicationContext(), "No more results for this search!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    imageResultsJson = response.getJSONObject("responseData").getJSONArray("results");
+                    imageResults.clear();
+
+                    aImageResults.addAll(ImageResult.fromJSONArray(imageResultsJson));
+                    if (aImageResults.getCount() < 8) {
+                        customLoadMoreDataFromApi(false);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.i("INFO", imageResults.toString());
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            // Creating intent
+            Intent i = new Intent(SearchActivity.this, ImageDisplayActivity.class);
+            Toast.makeText(this, "About to start the filters activity", Toast.LENGTH_SHORT).show();
+            String query = etQuery.getText().toString();
+            String searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8";
+            i.putExtra("searchUrl", searchUrl);
+            //Launch the activity
+            startActivity(i);
+            Toast.makeText(this, "Starting it", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
